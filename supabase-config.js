b7a22e01ac2e007b1b3af851da851587d8cbb4de
o1unicornlover <52230343+o1unicornlover@users.noreply.github.com>
2026-07-03 00:52:58 -0700

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://gqqssyqswyualglleedm.supabase.co";
+const SUPABASE_KEY = "sb_publishable_KaDiqAVR0_IdIuXW4mFyGg_8EHt2NWf";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);

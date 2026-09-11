02a230bf78f135ad5ff53f784cf191b49b3f7371
o1unicornlover <52230343+o1unicornlover@users.noreply.github.com>
2026-09-11 03:52:24 -0700

    Load clean appearance guard

--- script.js
+++ script.js
@@ -3,11 +3,12 @@
   Loader for the gradually modularized frontend.
 */
 (function loadAppScripts() {
-  const version = "stage2-ui40";
+  const version = "stage2-clean2";
   [
     "./js/constants.js",
     "./js/utils.js",
     "./js/legacy-app.js",
+    "./js/clean-appearance.js",
     "./js/autosync.js"
   ].forEach(src => {
     document.write(`<script src="${src}?v=${version}"><\/script>`);

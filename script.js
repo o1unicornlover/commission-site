aa158027c27846e972eef9b270335bc0402014a4
o1unicornlover <52230343+o1unicornlover@users.noreply.github.com>
2026-09-11 08:52:35 -0700

    Load admin routing cleanup module

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   Loader for the gradually modularized frontend.
 */
 (function loadAppScripts() {
-  const version = "stage2-clean7";
+  const version = "stage2-clean8";
   [
     "./js/constants.js",
     "./js/utils.js",
@@ -13,6 +13,7 @@
     "./js/runtime-stability.js",
     "./js/admin-app.js",
     "./js/admin-dashboard.js",
+    "./js/admin-routing.js",
     "./js/autosync.js"
   ].forEach(src => {
     document.write(`<script src="${src}?v=${version}"><\/script>`);

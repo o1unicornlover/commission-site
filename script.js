0712f06a14b627d01c1a11b5a41e900d62ab8aae
o1unicornlover <52230343+o1unicornlover@users.noreply.github.com>
2026-09-11 05:47:27 -0700

    Load theme studio and character carousel

--- script.js
+++ script.js
@@ -3,12 +3,13 @@
   Loader for the gradually modularized frontend.
 */
 (function loadAppScripts() {
-  const version = "stage2-clean2";
+  const version = "stage2-clean3";
   [
     "./js/constants.js",
     "./js/utils.js",
     "./js/legacy-app.js",
     "./js/clean-appearance.js",
+    "./js/site-customization.js",
     "./js/autosync.js"
   ].forEach(src => {
     document.write(`<script src="${src}?v=${version}"><\/script>`);

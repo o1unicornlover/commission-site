b5ddf6299bc9a3fbf485f42262f5814ee6b4c650
o1unicornlover <52230343+o1unicornlover@users.noreply.github.com>
2026-09-11 19:54:36 -0700

    Bump admin frontend build for alert preferences

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   Loader for the gradually modularized frontend.
 */
 (function loadAppScripts() {
-  const version = "stage2-clean17";
+  const version = "stage2-clean18";
   [
     "./js/constants.js",
     "./js/utils.js",

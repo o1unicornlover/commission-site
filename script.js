1d79e394def4c72d48251fab8bb47aa6550d0655
o1unicornlover <52230343+o1unicornlover@users.noreply.github.com>
2026-09-11 20:56:39 -0700

    Bump frontend build after admin cleanup

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   Loader for the gradually modularized frontend.
 */
 (function loadAppScripts() {
-  const version = "stage2-clean18";
+  const version = "stage2-clean19";
   [
     "./js/constants.js",
     "./js/utils.js",

112db2c6294a1ab46ae5b3fa376b6b233dcc7176
o1unicornlover <52230343+o1unicornlover@users.noreply.github.com>
2026-09-11 23:51:42 -0700

    Bump frontend after inbox reply workflow update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   Loader for the gradually modularized frontend.
 */
 (function loadAppScripts() {
-  const version = "stage2-clean21";
+  const version = "stage2-clean22";
   [
     "./js/constants.js",
     "./js/utils.js",

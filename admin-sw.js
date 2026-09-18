89fc17f4ae96dd53c2654508557d8a1aa41be3ca
o1unicornlover <52230343+o1unicornlover@users.noreply.github.com>
2026-09-18 13:52:52 -0700

    Refresh Admin PWA shell cache

--- admin-sw.js
+++ admin-sw.js
@@ -1,4 +1,4 @@
-const ADMIN_CACHE = "commission-admin-v98";
+const ADMIN_CACHE = "commission-admin-v99";
 const ADMIN_CACHE_PREFIX = "commission-admin-";
 const ADMIN_SHELL = [
   "./admin.html", "./style.css", "./admin-runtime.js", "./admin-manifest.webmanifest",
